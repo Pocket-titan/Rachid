8957d48809b2d38a7e26e97a099cd6298ab3f121
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2017-10-05 17:00:54 +0200

    youtube download :D

--- Rachid.js
+++ Rachid.js
@@ -1,24 +1,51 @@
-const Discord = require("discord.js")
+const Discord = require('discord.js')
+const ytdl = require('ytdl-core');
+const streamOptions = { seek: 0, volume: 1 };
 const client = new Discord.Client()
+const broadcast = client.createVoiceBroadcast();
+
 // change this when you clone rachid
 const token = 'MzY1MTA3OTk0MDMzMjU4NDk2.DLZg-w.XjW16JqgjvTaumV7Bb1q1_h2LDY'
 
 let channels = null
-let currentVoiceChannel = null
 
 const botsChannelID = '281062127928868864'
 
+const ytdlOptions = {
+  filter : 'audioonly',
+  quality: 'lowest',
+  highWaterMark: 16,
+
+}
 const send = (message, channel) => {
   channel.send(message)
     .then(message => console.log(`Sent message: ${message.content}`))
     .catch(console.error)
 }
 
-const playFile = (connection, filename) => {
-  const dispatcher = connection.playFile(filename)
-  dispatcher.on('end', () => {
-    dispatcher.destroy()
-  })
+const playFile = (voiceChannel, filename) => {
+  voiceChannel.join()
+    .then(connection => {
+      const dispatcher = connection.playFile(filename)
+      dispatcher.on('end', () => {
+        dispatcher.destroy()
+      })
+    })
+    .catch(console.error)
+}
+
+const playStream = (voiceChannel, link) => {
+  voiceChannel.join()
+    .then(connection => {
+      const stream = ytdl(link, ytdlOptions)
+        .on('error', err => console.log(err))
+      const dispatcher = connection.playStream(stream)
+      dispatcher.on('end', () => {
+        console.log('we done')
+        dispatcher.destroy()
+      })
+    })
+    .catch(console.error)
 }
 
 const findChannel = id => {
@@ -37,7 +64,14 @@ client.on('message', msg => {
 
   // trigger handling
   triggers.forEach(trigger => {
-    if (msg.content.toLowerCase().includes(trigger.q.toLowerCase())) {
+    let type = typeof trigger.q
+    // string or regex matching
+    match = (type === 'string')
+      ? msg.content.toLowerCase().match(trigger.q.toLowerCase())
+      : msg.content.match(trigger.q)
+
+    // if we match shit
+    if (match && match[0].length > 2) {
       if (msg.author.bot) {
         return
       }
@@ -46,6 +80,9 @@ client.on('message', msg => {
       }
       else if (trigger.type === 'voice') {
         trigger.a(findChannel(msg.author.lastMessage.member.voiceChannelID))
+      }
+      else if (trigger.type === 'url') {
+        trigger.a(findChannel(msg.author.lastMessage.member.voiceChannelID), match[0])
       }
     }
   })
@@ -63,54 +100,68 @@ stdin.addListener("data", d => {
 const triggers = [
   {
     q: 'why are we still here',
-    a: channel => {
-      send('just to suffer?', channel)
-    },
+    a: channel => {send('just to suffer?', channel)},
     type: 'text',
   },
   {
     q: 'rachid wie ben je',
-    a: channel => {
-      send('ik ben rachid', channel)
-    },
+    a: channel => {send('ik ben rachid', channel)},
     type: 'text',
   },
   {
     q: 'rachid kom',
-    a: voiceChannel => {
-      voiceChannel.join()
-        .then(connection => playFile(connection, './mp3/ikbenrachid.mp3'))
-        .catch(console.error)
-    },
+    a: voiceChannel => {playFile(voiceChannel, audio[this.q])},
     type: 'voice',
   },
   {
     q: 'rachid heb je even voor mij',
-    a: voiceChannel => {
-      voiceChannel.join()
-        .then(connection => playFile(connection, './mp3/hebjeevenvoormij.mp3'))
-        .catch(console.error)
-    },
+    a: voiceChannel => {playFile(voiceChannel, audio[this.q])},
     type: 'voice',
   },
   {
     q: 'rachid fatoe',
-    a: voiceChannel => {
-      voiceChannel.join()
-        .then(connection => playFile(connection, './mp3/fatoe.mp3'))
-        .catch(console.error)
-    },
+    a: voiceChannel => {playFile(voiceChannel, audio[this.q])},
     type: 'voice',
   },
   {
     q: 'rachid ga weg',
-    a: voiceChannel => {
-      if (voiceChannel) {
-          voiceChannel.leave()
-      }
-    },
+    a: voiceChannel => {if (voiceChannel) {voiceChannel.leave()}},
     type: 'voice',
-  }
+  },
+  {
+    q: 'rachid kanker',
+    a: voiceChannel => {playFile(voiceChannel, audio[this.q])},
+    type: 'voice',
+  },
+  {
+    q: 'rachid 5 euro',
+    a: voiceChannel => {playFile(voiceChannel, audio[this.q])},
+    type: 'voice',
+  },
+  {
+    q: /^rachid (?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/,
+    a: (voiceChannel, url) => {
+      let match = validateYouTubeUrl(url)
+      if (match) {playStream(voiceChannel, match)}
+    },
+    type: 'url',
+  },
 ]
+
+let validateYouTubeUrl = url => {
+    if (url != undefined || url != '') {
+        let regExp = /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/
+        let match = url.match(regExp)
+        return (match && match[1].length == 11) ? match[0] : false
+    }
+}
+
+const audio = {
+  'rachid kom': 'ikbenrachid.mp3',
+  'rachid heb je even voor mij': 'hebjeevenvoormij.mp3',
+  'rachid fatoe': 'fatoe.mp3',
+  'rachid kanker': 'kanker.mp3',
+  'rachid 5 euro': '5euro.mp3',
+}
 
 client.login(token)
