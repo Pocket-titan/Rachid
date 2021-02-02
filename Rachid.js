0134c47e22c570605e611a9d5983921384b0ef42
Jelmar Gerritsen <jelmargerritsen@gmail.com>
2021-02-02 18:29:17 +0100

    Fix rachid

--- Rachid.js
+++ Rachid.js
@@ -1,16 +1,16 @@
-import search from 'youtube-search';
-import Discord from 'discord.js';
-import ytdl from 'ytdl-core';
-import { token, youtube_api_key } from './auth.json';
+import search from "youtube-search";
+import Discord from "discord.js";
+import ytdl from "ytdl-core";
+import { token, youtube_api_key } from "./auth.json";
 
 const client = new Discord.Client();
 const broadcast = client.createVoiceBroadcast();
 
-const mdwekmegID = '240902201868812289';
-const pppppID = '689084892692086784';
+const mdwekmegID = "240902201868812289";
+const pppppID = "689084892692086784";
 const server_ID_to_operate_in = pppppID;
 
-const rachidID = '365107994033258496';
+const rachidID = "365107994033258496";
 
 let channels = [];
 let voice_connection = null;
@@ -32,6 +32,7 @@ const join_channel = async (voice_channel) => {
       return voice_connection;
     }
   }
+
   let connection = await voice_channel.join();
   voice_connection = connection;
 
@@ -47,7 +48,7 @@ const stopPlaying = async () => {
 
 const playFile = async (voice_channel, filename, user_options = {}) => {
   if (!voice_channel) {
-    console.log('Not in a voice channel');
+    console.log("Not in a voice channel");
     return;
   }
   console.log(`Playing file ${filename}`);
@@ -64,9 +65,12 @@ const playFile = async (voice_channel, filename, user_options = {}) => {
     ...user_options,
   };
 
-  const dispatcher = (await join_channel(voice_channel)).playFile(`./mp3/${filename}`, options);
+  const dispatcher = (await join_channel(voice_channel)).playFile(
+    `./mp3/${filename}`,
+    options
+  );
 
-  dispatcher.on('finish', () => {
+  dispatcher.on("finish", () => {
     currently_playing = null;
   });
 
@@ -75,8 +79,8 @@ const playFile = async (voice_channel, filename, user_options = {}) => {
 
 const playStream = async (voice_channel, link, user_options = {}) => {
   const default_ytdl_options = {
-    filter: 'audioonly',
-    quality: 'highest',
+    filter: "audioonly",
+    quality: "highest",
   };
 
   const ytdl_options = {
@@ -84,27 +88,31 @@ const playStream = async (voice_channel, link, user_options = {}) => {
     ...user_options,
   };
 
-  const stream = ytdl(link, ytdl_options).on('error', (err) => console.log(err));
+  const stream = ytdl(link, ytdl_options).on("error", (err) => console.log(err));
 
-  const dispatcher = (await join_channel(voice_channel)).playStream(stream, { volume: volume / 100 });
+  const dispatcher = (await join_channel(voice_channel)).playStream(stream, {
+    volume: volume / 100,
+  });
 
-  dispatcher.on('finish', () => {
+  dispatcher.on("finish", () => {
     currently_playing = null;
   });
 
   currently_playing = dispatcher;
 };
 
-const find_channel = (id) => channels.find((channel) => channel.id === id);
+const find_channel = (id) => {
+  return channels.find((channel) => channel.id === id);
+};
 
-client.on('ready', async () => {
+client.on("ready", async () => {
   console.log(`ik ben ${client.user.tag}`);
   const { guilds } = client;
   let guild = guilds.get(server_ID_to_operate_in);
   channels.push(...Array.from(guild.channels.values()));
 
   const current_voice_channels = client.channels
-    .filter(({ type }) => type === 'voice')
+    .filter(({ type }) => type === "voice")
     .filter(({ members }) => members.some(({ user }) => user.id === rachidID))
     .map(({ id }) => id);
   const already_in_voice_channel = current_voice_channels.length > 0;
@@ -118,17 +126,17 @@ client.on('ready', async () => {
   }
 });
 
-client.on('message', async (message) => {
-  const emoji = (name) => message.guild.emojis.find('name', name);
+client.on("message", async (message) => {
+  const emoji = (name) => message.guild.emojis.find("name", name);
 
   try {
     // trigger handling
     for (let trigger of triggers) {
       // string or regex matching
       let match =
-        typeof trigger.q === 'string'
+        typeof trigger.q === "string"
           ? message.content.toLowerCase().match(trigger.q.toLowerCase())
-          : typeof trigger.q === 'function'
+          : typeof trigger.q === "function"
           ? trigger.q(message.content.toLowerCase())
           : message.content.match(trigger.q);
 
@@ -144,15 +152,15 @@ client.on('message', async (message) => {
 
         // there's different trigger types
         switch (trigger.type) {
-          case 'text':
+          case "text":
             return await trigger.a(message.channel);
-          case 'voice':
+          case "voice":
             return await trigger.a(voice_channel, trigger.q);
-          case 'url':
+          case "url":
             return await trigger.a(voice_channel, match[0]);
-          case 'search':
-            return await trigger.a(voice_channel, match['input']);
-          case 'volume':
+          case "search":
+            return await trigger.a(voice_channel, match["input"]);
+          case "volume":
             return await trigger.a(match[1]);
           default:
             return await trigger.a();
@@ -166,16 +174,16 @@ client.on('message', async (message) => {
 });
 
 // Ctrl+c makes rachid leave the call (otherwise bugs)
-process.on('SIGINT', () => {
+process.on("SIGINT", () => {
   client.destroy();
   process.exit();
 });
 
 // rachid can talk
 let stdin = process.openStdin();
-stdin.addListener('data', (d) => {
+stdin.addListener("data", (d) => {
   let message = d.toString().trim();
-  let bots_channel = channels.find((channel) => channel.name.includes('bots'));
+  let bots_channel = channels.find((channel) => channel.name.includes("bots"));
   if (bots_channel) {
     send(message, bots_channel);
   }
@@ -185,113 +193,115 @@ stdin.addListener('data', (d) => {
 // an action that rachid will do on receiving the question
 const triggers = [
   {
-    q: 'why are we still here',
-    a: (channel) => send('just to suffer?', channel),
-    type: 'text',
+    q: "why are we still here",
+    a: (channel) => send("just to suffer?", channel),
+    type: "text",
   },
   {
-    q: 'rachid wie ben je',
-    a: (channel) => send('ik ben rachid', channel),
-    type: 'text',
+    q: "rachid wie ben je",
+    a: (channel) => send("ik ben rachid", channel),
+    type: "text",
   },
   {
-    q: 'rachid kom',
-    a: (voice_channel, q) => playFile(voice_channel, 'ikbenrachid.mp3', { seek: 51, volume: 0.03 }),
-    type: 'voice',
+    q: "rachid kom",
+    a: (voice_channel, q) =>
+      playFile(voice_channel, "ikbenrachid.mp3", { seek: 51, volume: 0.07 }),
+    type: "voice",
   },
   {
-    q: 'rachid heb je even voor mij',
-    a: (voice_channel, q) => playFile(voice_channel, 'hebjeevenvoormij.mp3', { volume: 0.01 }),
-    type: 'voice',
+    q: "rachid heb je even voor mij",
+    a: (voice_channel, q) =>
+      playFile(voice_channel, "hebjeevenvoormij.mp3", { volume: 0.01 }),
+    type: "voice",
   },
   {
-    q: 'rachid fatoe',
-    a: (voice_channel, q) => playFile(voice_channel, 'fatoe.mp3', { volume: 0.1 }),
-    type: 'voice',
+    q: "rachid fatoe",
+    a: (voice_channel, q) => playFile(voice_channel, "fatoe.mp3", { volume: 0.1 }),
+    type: "voice",
   },
   {
     q: /^rachid klep (([1-9][0-9]*)|ff)/,
     a: (number) => {
-      if (number == 'ff') number = 0;
+      if (number == "ff") number = 0;
       volume = number;
       if (currently_playing) {
         // setVolume takes 1 for normal vol, 0.5 for half, 2 for double :/
         currently_playing.setVolume(volume / 100);
       }
     },
-    type: 'volume',
+    type: "volume",
   },
   {
-    q: 'rachid help',
+    q: "rachid help",
     a: (channel) =>
       send(
-        triggers.map((trigger) => '`' + trigger.q + '`'),
+        triggers.map((trigger) => "`" + trigger.q + "`"),
         channel
       ),
-    type: 'text',
+    type: "text",
   },
   {
     q: (message) => {
-      if (['rachid', 'ga'].every((word) => message.includes(word))) {
-        return [['rachid', 'ga', 'weg']];
+      if (["rachid", "ga"].every((word) => message.includes(word))) {
+        return [["rachid", "ga", "weg"]];
       }
 
       return null;
     },
     a: (voice_channel) => {
-      console.log('doei');
+      console.log("doei");
 
       if (voice_connection) {
         voice_connection.disconnect();
         voice_connection = null;
       }
     },
-    type: 'voice',
+    type: "voice",
   },
   {
-    q: 'rachid kankerheet',
-    a: (voice_channel, q) => playFile(voice_channel, 'kankerheet.mp3', { volume: 0.1 }),
-    type: 'voice',
+    q: "rachid kankerheet",
+    a: (voice_channel, q) => playFile(voice_channel, "kankerheet.mp3", { volume: 0.1 }),
+    type: "voice",
   },
   {
-    q: 'rachid kanker',
-    a: (voice_channel, q) => playFile(voice_channel, 'kanker.mp3', { volume: 0.2 }),
-    type: 'voice',
+    q: "rachid kanker",
+    a: (voice_channel, q) => playFile(voice_channel, "kanker.mp3", { volume: 0.2 }),
+    type: "voice",
   },
   {
-    q: 'rachid bolt',
-    a: (voice_channel, q) => playFile(voice_channel, 'bolt.mp3', { volume: 0.2 }),
-    type: 'voice',
+    q: "rachid bolt",
+    a: (voice_channel, q) => playFile(voice_channel, "bolt.mp3", { volume: 0.2 }),
+    type: "voice",
   },
   {
-    q: 'rachid 5 euro',
-    a: (voice_channel, q) => playFile(voice_channel, '5euro.mp3'),
-    type: 'voice',
+    q: "rachid 5 euro",
+    a: (voice_channel, q) => playFile(voice_channel, "5euro.mp3"),
+    type: "voice",
   },
   {
-    q: 'rachid bitconnect',
-    a: (voice_channel, q) => playFile(voice_channel, 'bitconnect.mp3'),
-    type: 'voice',
+    q: "rachid bitconnect",
+    a: (voice_channel, q) => playFile(voice_channel, "bitconnect.mp3"),
+    type: "voice",
   },
   {
-    q: 'rachid sonata',
-    a: (voice_channel, q) => playFile(voice_channel, 'bitonnect_sonata.mp3'),
-    type: 'voice',
+    q: "rachid sonata",
+    a: (voice_channel, q) => playFile(voice_channel, "bitonnect_sonata.mp3"),
+    type: "voice",
   },
   {
-    q: 'rachid hey',
-    a: (voice_channel, q) => playFile(voice_channel, 'hey.mp3'),
-    type: 'voice',
+    q: "rachid hey",
+    a: (voice_channel, q) => playFile(voice_channel, "hey.mp3"),
+    type: "voice",
   },
   {
-    q: 'rachid wusup',
-    a: (voice_channel, q) => playFile(voice_channel, 'wusup.mp3'),
-    type: 'voice',
+    q: "rachid wusup",
+    a: (voice_channel, q) => playFile(voice_channel, "wusup.mp3"),
+    type: "voice",
   },
   {
-    q: 'rachid nonono',
-    a: (voice_channel, q) => playFile(voice_channel, 'nonono.mp3'),
-    type: 'voice',
+    q: "rachid nonono",
+    a: (voice_channel, q) => playFile(voice_channel, "nonono.mp3"),
+    type: "voice",
   },
   {
     q: /^rachid (?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/,
@@ -300,7 +310,7 @@ const triggers = [
       let match = url.match(regex);
       if (match && match[1].length == 11) playStream(voice_channel, match[0]);
     },
-    type: 'url',
+    type: "url",
   },
   {
     q: /^rachid play .*/,
@@ -309,19 +319,22 @@ const triggers = [
         maxResults: 5,
         key: youtube_api_key,
       };
-      search(query.replace('rachid play', '').trim(), options, (err, results) => {
+      search(query.replace("rachid play", "").trim(), options, (err, results) => {
         if (err) return console.log(err);
-        playStream(voice_channel, results.filter((result) => result.kind === 'youtube#video')[0].link);
+        playStream(
+          voice_channel,
+          results.filter((result) => result.kind === "youtube#video")[0].link
+        );
       });
     },
-    type: 'search',
+    type: "search",
   },
   {
     q: /^rachid s+h*($|s)/,
     a: () => stopPlaying(),
   },
   {
-    q: 'rachid stop',
+    q: "rachid stop",
     a: () => stopPlaying(),
   },
 ];
